5db02ed656fbb5e02a8263a435539612ff8f2ed4
Guinter Weber <guinter.weber@digitaldesk.com.br>
2016-05-30 16:06:42 -0300

    botoes e selects de caminhao

--- Js/javascript.js
+++ Js/javascript.js
@@ -1,12 +1,14 @@
 var urlPadrao = "http://fipeapi.appspot.com/api/1/";
 
 $(document).ready(function(){
-	$(".cars").show();
-	$(".motorcycles").show();
-	$(".trucks").show();
-	$("#preenchimento").hide();
+	inicio();
 	cliques();
 });
+
+	function inicio(){
+		$(".cars, .motorcycles, .trucks").show();
+		$("#preenchimento").hide();
+	}
 
 	function cliques(){
 		$(".cars").click(function(){
@@ -21,26 +23,63 @@ $(document).ready(function(){
 	}
 
 	function Carros(){
-//		$(".list-group").hide();
 		$("#preenchimento").show();
-		$(".list-group,#counteiner,#motos,#caminhoes").hide();
-		$("#BotaoSelecionar").click(function(){
-			
-		})
+		$(".list-group, #counteiner, #motos, #caminhoes").hide();
+		RequesicaoMarcas(urlPadrao + 'carros/marcas.json');
 	}
 
 	function Motos(){
-//		$(".list-group").hide();
 		$("#preenchimento").show();
-		$(".list-group,#counteiner,#carros,#caminhoes").hide();
+		$(".list-group, #counteiner, #carros, #caminhoes").hide();
+		RequesicaoMarcas(urlPadrao + 'motos/marcas.json');
 	}
 
 	function Caminhoes(){
-//		$(".list-group").hide();
 		$("#preenchimento").show();
-		$(".list-group,#counteiner,#motos,#carros").hide();
+		$(".list-group, .Modelo, .BotaoSelecionarModelo, #counteiner, #motos, #carros").hide();
+		MarcasCaminhoes();
 	}
 
+	function MarcasCaminhoes(){
+		RequesicaoMarcas(urlPadrao + 'caminhoes/marcas.json');
+		var k= $('.Marca').val();
+		$(".BotaoSelecionarMarcas").click(function(){
+			ModelosCaminhoes(k);
+		})
+	}
+
+	function ModelosCaminhoes(k){
+		$(".Modelo").show();
+		RequesicaoModelo(urlPadrao + 'caminhoes/veiculos/21.json');
+		var l= $('.Modelo').val();
+		$(".BotaoSelecionarModeloo").click(function(){
+
+		})
+	}
+
+	function RequesicaoModelo(endereco){
+		$.getJSON(endereco, function(database){
+			AtualizarSelectModelos();
+		})
+	}
+
+	function RequesicaoMarcas(endereco){
+		$.getJSON(endereco, function(database){
+			AtualizarSelectMarcas(database);
+		})
+	}
+
+	function AtualizarSelectModelos(){
+
+	}
+
+	function AtualizarSelectMarcas(database){
+		var alternativas='';
+		for (var g=0; g<database.length; g++){
+			alternativas+='<option value=' + g + '>' + database[g].name + '</option>';
+		}
+		$(".Marca").html(alternativas);
+	}
 /*função que testa a variável 'z' para que a primeira opção não seja escolhida.
 	function TesteVar(z){
 		var z = $('#Select').val();
