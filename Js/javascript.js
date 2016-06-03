50e437d13fa1e8e64656047dbacf7b0239804660
Guinter Weber <guinter.weber@digitaldesk.com.br>
2016-06-03 14:21:38 -0300

    Criar funções padroes #4

--- Js/javascript.js
+++ Js/javascript.js
@@ -12,116 +12,125 @@ $(document).ready(function(){
 
 	function Cliques(){
 		$("#cars").click(function(){
-			console.log('carros');
 			Carros();
 		});
 		$("#motorcycles").click(function(){
-			console.log('motos');
 			Motos();
 		});
 		$("#trucks").click(function(){
-			console.log('caminhoes');
 			Caminhoes();
 		});
 	}
 
 	function Carros(){
-		$("#FuncoesEspecificas,#MarcaCarro").show();
-		$(".PrimeiroQuadro,.BotaoSelecionarModelo,#ModeloCarro,.BotaoSelecionarAno,#AnoCarro,#escritas,.motos,.caminhoes").hide();
-		MarcasCarros();
+		$("#FuncoesEspecificas,.carros").show();
+		$(".PrimeiroQuadro,.BotaoSelecionarModelo,.Modelo,.BotaoSelecionarAno,.Ano,.motos,.caminhoes,#escritas").hide();
+		var tipo='carros';
+		Marcas(tipo);
 	}
 
 	function Motos(){
-		$("#FuncoesEspecificas,#MarcaMoto").show();
-		$(".PrimeiroQuadro,.BotaoSelecionarModelo,#ModeloMoto,.BotaoSelecionarAno,#AnoMoto,#escritas,.carros,.caminhoes").hide();
-		MarcasMotos();
+		$("#FuncoesEspecificas,.motos").show();
+		$(".PrimeiroQuadro,.BotaoSelecionarModelo,.Modelo,.BotaoSelecionarAno,.Ano,.carros,.caminhoes,#escritas").hide();
+		var tipo='motos';
+		Marcas(tipo);
 	}
 
 	function Caminhoes(){
-		$("#FuncoesEspecificas,#MarcaCaminhao").show();
-		$(".PrimeiroQuadro,.BotaoSelecionarModelo,#ModeloCaminhao,.BotaoSelecionarAno,#AnoCaminhao,#ModeloCaminhao,#escritas,.motos,.carros").hide();
-		MarcasCaminhoes();
+		$("#FuncoesEspecificas,.caminhoes").show();
+		$(".PrimeiroQuadro,.BotaoSelecionarModelo,.Modelo,.BotaoSelecionarAno,.Ano,.motos,.carros,#escritas").hide();
+		var tipo='caminhoes';
+		Marcas(tipo);
 	}
-
-//FUNÇÕES REFERENTES AOS CARROS.
-
-	function MarcasCarros(){
-		RequesicaoMarcasCarros(urlPadrao + 'carros/marcas.json');
+	
+	function Marcas(tipo){
+		RequesicaoMarcas(urlPadrao + tipo + '/marcas.json');
 		$(".BotaoSelecionarMarcas").unbind();
 		$(".BotaoSelecionarMarcas").click(function(){
-			var car= $('#MarcaCarro').val();
-			ModelosCarros(car);
+			var tipo2= $('.Marca').val();
+			Modelos(tipo,tipo2);
 		})
 	}
-	function ModelosCarros(car){
-		$("#ModeloCarro,.BotaoSelecionarModelo").show();
-			RequesicaoModelosCarros(urlPadrao + 'carros/veiculos/' + car + '.json');
-			$('#MarcaCarro').on('change', function() {
-				$("#ModeloCarro,.BotaoSelecionarModelo,#AnoCarro,.BotaoSelecionarAno").hide();			
+
+	function Modelos(tipo,tipo2){
+		$(".Modelo,.BotaoSelecionarModelo").show();
+			RequesicaoModelos(urlPadrao + tipo + '/veiculos/' + tipo2 + '.json');
+			$('.Marca').on('change', function() {
+				$(".Modelo,.BotaoSelecionarModelo,.Ano,.BotaoSelecionarAno").hide();			
 			})
-			$(".BotaoSelecionarModelo").unbind();
+		$(".BotaoSelecionarModelo").unbind();
 		$(".BotaoSelecionarModelo").click(function(){
-			var car2= $('#ModeloCarro').val();
-			AnosCarros(car,car2);
+			var tipo3= $('.Modelo').val();
+			Anos(tipo,tipo2,tipo3);
 		})
 	}
-	function AnosCarros(car,car2){
-		$("#AnoCarro,.BotaoSelecionarAno").show();
-			RequesicaoAnosCarros(urlPadrao + 'carros/veiculo/' + car + '/' + car2 + '.json');
-			$('#ModeloCarro').on('change', function() {
-				$("#AnoCarro,.BotaoSelecionarAno").hide();			
+
+	function Anos(tipo,tipo2,tipo3){
+		$(".Ano,.BotaoSelecionarAno").show();
+			RequesicaoAnos(urlPadrao + tipo + '/veiculo/' + tipo2 + '/' + tipo3 + '.json');
+			$('.Modelo').on('change', function() {
+				$(".Ano,.BotaoSelecionarAno").hide();			
 			})
 		$(".BotaoSelecionarAno").unbind();
 		$(".BotaoSelecionarAno").click(function(){
-			var car3= $('#AnoCarro').val();
-			CarroSelecionado(car,car2,car3);
+			var tipo4= $('.Ano').val();
+			VeiculoSelecionado(tipo,tipo2,tipo3,tipo4);
 		})
 	}
-	function CarroSelecionado(car,car2,car3){
-		RequisicaoCarroEscolhido(urlPadrao + 'carros/veiculo/' + car + '/' + car2 + '/' + car3 + '.json');
+
+	function VeiculoSelecionado(tipo,tipo2,tipo3,tipo4){
+		RequisicaoVeiculoEscolhido(urlPadrao + tipo + '/veiculo/' + tipo2 + '/' + tipo3 + '/' + tipo4 + '.json');
 	}
-	function RequesicaoMarcasCarros(endereco){
+	
+	function RequesicaoMarcas(endereco){
 		$.getJSON(endereco, function(database){
-			AtualizarSelectMarcasCarros(database);
+			AtualizarSelectMarcas(database);
 		})
 	}
-	function AtualizarSelectMarcasCarros(database){
+
+	function AtualizarSelectMarcas(database){
 		var alternativas='';
 			for (var g=0; g<database.length; g++){
 				alternativas+='<option value=' + database[g].id + '>' + database[g].name + '</option>';
 			}
-		$("#MarcaCarro").html(alternativas);
+		$(".Marca").html(alternativas);
 	}
-	function RequesicaoModelosCarros(endereco){
+
+	function RequesicaoModelos(endereco){
 		$.getJSON(endereco, function(database){
-			AtualizarSelectModelosCarros(database);
+			AtualizarSelectModelos(database);
 		})
 	}
-	function AtualizarSelectModelosCarros(database){
+
+	function AtualizarSelectModelos(database){
 		var alternativas='';
 		for (var g=0; g<database.length; g++){
 			alternativas+='<option value=' + database[g].id + '>' + database[g].name + '</option>';
 		}
-		$("#ModeloCarro").html(alternativas);
+		$(".Modelo").html(alternativas);
 	}
-	function RequesicaoAnosCarros(endereco){
+
+	function RequesicaoAnos(endereco){
 		$.getJSON(endereco, function(database){
-			AtualizarSelectAnoCarro(database);
+			AtualizarSelectAno(database);
 		})
 	}
-	function AtualizarSelectAnoCarro(database){
+
+	function AtualizarSelectAno(database){
 		var alternativas='';
 		for (var g=0; g<database.length; g++){
 			alternativas+='<option value=' + database[g].id + '>' + database[g].name + '</option>';
 		}
-		$("#AnoCarro").html(alternativas);
+		$(".Ano").html(alternativas);
 	}
-	function RequisicaoCarroEscolhido(endereco){
+
+	function RequisicaoVeiculoEscolhido(endereco){
 		$.getJSON(endereco, function(database){
-			CarroEscolhido(database);
+			VeiculoEscolhido(database);
 		})
 	}
-	function CarroEscolhido(database){
+
+	function VeiculoEscolhido(database){
 		var alternativas='';
 		for (var g=0; g<database.length; g++){
 			alternativas+='<option value=' + database[g].id + '>' +  database[g].name + '</option>';
@@ -129,179 +138,22 @@ $(document).ready(function(){
 		TabelaResultados(database);
 	}
 
-//FUNÇÕES REFERENTES AS MOTOS.
+	function DataPesquisa(){ 
+		var momentoAtual = new Date();
+		var vhora = momentoAtual.getHours();
+		if (vhora < 10){ vhora = "0" + vhora;}
+		var vminuto = momentoAtual.getMinutes();		
+		if (vminuto < 10){ vminuto = "0" + vminuto;}
+		var vsegundo = momentoAtual.getSeconds();
+		if (vsegundo < 10){ vsegundo = "0" + vsegundo;}
+		var vdia = momentoAtual.getDate();		
+		if (vdia < 10){ vdia = "0" + vdia;}
+		var vmes = momentoAtual.getMonth() + 1;		
+		if (vmes < 10){ vmes = "0" + vmes;}
+		var vano = momentoAtual.getFullYear();
+ 			horario = vdia + "/" + vmes + "/" + vano + ' - ' + vhora + ":" + vminuto + ":" + vsegundo + '<br>';
+ 	}
 
-	function MarcasMotos(){
-		RequesicaoMarcasMotos(urlPadrao + 'motos/marcas.json');
-		$(".BotaoSelecionarMarcas").unbind();
-		$(".BotaoSelecionarMarcas").click(function(){
-			var moto= $('#MarcaMoto').val();
-			ModelosMotos(moto);
-		})
-	}
-	function ModelosMotos(moto){
-		$("#ModeloMoto, .BotaoSelecionarModelo").show();
-			RequesicaoModelosMotos(urlPadrao + 'motos/veiculos/' + moto + '.json');
-			$('#MarcaMoto').on('change', function() {
-				$("#ModeloMoto,.BotaoSelecionarModelo,#AnoMoto,.BotaoSelecionarAno").hide();			
-			})
-		$(".BotaoSelecionarModelo").unbind();
-		$(".BotaoSelecionarModelo").click(function(){
-			var moto2= $('#ModeloMoto').val();
-			AnoMoto(moto,moto2);
-		})
-	}
-	function AnoMoto(moto,moto2){
-		$("#AnoMoto,.BotaoSelecionarAno").show();
-			RequesicaoAnosMotos(urlPadrao + 'motos/veiculo/' + moto + '/' + moto2 + '.json');
-			$('#ModeloMoto').on('change', function() {
-				$("#AnoMoto,.BotaoSelecionarAno").hide();			
-			})
-		$(".BotaoSelecionarAno").unbind();
-		$(".BotaoSelecionarAno").click(function(){
-			var moto3= $('#AnoMoto').val();
-			MotoSelecionada(moto,moto2,moto3);
-		})
-	}
-	function MotoSelecionada(moto,moto2,moto3){
-		RequisicaoMotoEscolhida(urlPadrao + 'motos/veiculo/' + moto + '/' + moto2 + '/' + moto3 + '.json');
-	}
-	function RequesicaoMarcasMotos(endereco){
-		$.getJSON(endereco, function(database){
-			AtualizarSelectMarcasMotos(database);
-		})
-	}
-	function AtualizarSelectMarcasMotos(database){
-		var alternativas='';
-		for (var g=0; g<database.length; g++){
-			alternativas+='<option value=' + database[g].id + '>' + database[g].name + '</option>';
-		}
-		$("#MarcaMoto").html(alternativas);
-	}
-	function RequesicaoModelosMotos(endereco){
-		$.getJSON(endereco, function(database){
-			AtualizarSelectModelosMotos(database);
-		})
-	}
-	function AtualizarSelectModelosMotos(database){
-		var alternativas='';
-		for (var g=0; g<database.length; g++){
-			alternativas+='<option value=' + database[g].id + '>' + database[g].name + '</option>';
-		}
-		$("#ModeloMoto").html(alternativas);
-	}
-	function RequesicaoAnosMotos(endereco){
-		$.getJSON(endereco, function(database){
-			AtualizarSelectAnoMoto(database);
-		})
-	}
-	function AtualizarSelectAnoMoto(database){
-		var alternativas='';
-		for (var g=0; g<database.length; g++){
-			alternativas+='<option value=' + database[g].id + '>' + database[g].name + '</option>';
-		}
-		$("#AnoMoto").html(alternativas);
-	}
-	function RequisicaoMotoEscolhida(endereco){
-		$.getJSON(endereco, function(database){
-			MotoEscolhida(database);
-		})
-	}
-	function MotoEscolhida(database){
-		var alternativas='';
-		for (var g=0; g<database.length; g++){
-			alternativas+='<option value=' + database[g].id + '>' +  database[g].name + '</option>';
-		}
-		TabelaResultados(database);
-	}
-
-//FUNÇÕES REFERENTES AOS CAMINHÕES.
-
-	function MarcasCaminhoes(){
-		RequesicaoMarcasCaminhoes(urlPadrao + 'caminhoes/marcas.json');
-		$(".BotaoSelecionarMarcas").unbind();
-		$(".BotaoSelecionarMarcas").click(function(){
-			var truck= $('#MarcaCaminhao').val();
-			ModelosCaminhoes(truck);
-		})
-	}
-	function ModelosCaminhoes(truck){
-		$("#ModeloCaminhao,.BotaoSelecionarModelo").show();
-			RequesicaoModelosCaminhoes(urlPadrao + 'caminhoes/veiculos/' + truck + '.json');
-			$('#MarcaCaminhao').on('change', function() {
-				$("#ModeloCaminhao,.BotaoSelecionarModelo,#AnoCaminhao,.BotaoSelecionarAno").hide();			
-			})
-		$(".BotaoSelecionarModelo").unbind();
-		$(".BotaoSelecionarModelo").click(function(){
-			var truck2= $('#ModeloCaminhao').val();
-			AnoCaminhao(truck,truck2);
-		})
-	}
-	function AnoCaminhao(truck,truck2){
-		$("#AnoCaminhao,.BotaoSelecionarAno").show();
-			RequesicaoAnosCaminhoes(urlPadrao + 'caminhoes/veiculo/' + truck + '/' + truck2 + '.json');
-			$('#ModeloCaminhao').on('change', function() {
-				$("#AnoCaminhao,.BotaoSelecionarAno").hide();			
-			})
-		$(".BotaoSelecionarAno").unbind();
-		$(".BotaoSelecionarAno").click(function(){
-			var truck3= $('#AnoCaminhao').val();
-			CaminhaoSelecionado(truck,truck2,truck3);
-		})
-	}
-	function CaminhaoSelecionado(truck,truck2,truck3){
-		RequisicaoCaminhaoEscolhido(urlPadrao + 'caminhoes/veiculo/' + truck + '/' + truck2 + '/' + truck3 + '.json');
-	}
-	function RequesicaoMarcasCaminhoes(endereco){
-		$.getJSON(endereco, function(database){
-			AtualizarSelectMarcasCaminhoes(database);
-		})
-	}
-	function AtualizarSelectMarcasCaminhoes(database){
-		var alternativas='';
-		for (var g=0; g<database.length; g++){
-			alternativas+='<option value=' + database[g].id + '>' + database[g].name + '</option>';
-		}
-		$("#MarcaCaminhao").html(alternativas);
-	}
-	function RequesicaoModelosCaminhoes(endereco){
-		$.getJSON(endereco, function(database){
-			AtualizarSelectModelosCaminhoes(database);
-		})
-	}
-	function AtualizarSelectModelosCaminhoes(database){
-		var alternativas='';
-		for (var g=0; g<database.length; g++){
-			alternativas+='<option value=' + database[g].id + '>' + database[g].name + '</option>';
-		}
-		$("#ModeloCaminhao").html(alternativas);
-	}
-	function RequesicaoAnosCaminhoes(endereco){
-		$.getJSON(endereco, function(database){
-			AtualizarSelectAnoCaminhao(database);
-		})
-	}
-	function AtualizarSelectAnoCaminhao(database){
-		var alternativas='';
-		for (var g=0; g<database.length; g++){
-			alternativas+='<option value=' + database[g].id + '>' + database[g].name + '</option>';
-		}
-		$("#AnoCaminhao").html(alternativas);
-	}
-	function RequisicaoCaminhaoEscolhido(endereco){
-		$.getJSON(endereco, function(database){
-			CaminhaoEscolhido(database);
-		})
-	}
-	function CaminhaoEscolhido(database){
-		var alternativas='';
-		for (var g=0; g<database.length; g++){
-			alternativas+='<option value=' + database[g].id + '>' +  database[g].name + '</option>';
-		}
-		TabelaResultados(database);
-	}
-
-//TABELA COM OS RESULTADOS.
 	function TabelaResultados(database){
 		$(".table-responsive,.AdicaoEdicao").show();
 		$("#FuncoesEspecificas").hide();
@@ -318,20 +170,3 @@ $(document).ready(function(){
 			Inicio();
 		});
 	}
-
-//FUNÇÃO  DO HORARIO DA CONSULTA.
-	function DataPesquisa(){ 
-		var momentoAtual = new Date();
-		var vhora = momentoAtual.getHours();
-		if (vhora < 10){ vhora = "0" + vhora;}
-		var vminuto = momentoAtual.getMinutes();		
-		if (vminuto < 10){ vminuto = "0" + vminuto;}
-		var vsegundo = momentoAtual.getSeconds();
-		if (vsegundo < 10){ vsegundo = "0" + vsegundo;}
-		var vdia = momentoAtual.getDate();		
-		if (vdia < 10){ vdia = "0" + vdia;}
-		var vmes = momentoAtual.getMonth() + 1;		
-		if (vmes < 10){ vmes = "0" + vmes;}
-		var vano = momentoAtual.getFullYear();
- 			horario = vdia + "/" + vmes + "/" + vano + ' - ' + vhora + ":" + vminuto + ":" + vsegundo + '<br>';
- 	}
